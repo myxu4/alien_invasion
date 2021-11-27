8ce4ebe59171509fe85472022106335410bb0dec
Sergey <myxu4@bk.ru>
2021-11-28 00:14:20 +0300

    Цвет окна

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,9 +3,11 @@ import pygame
 class AlienInvasion:
     def __init__(self):
 
+
         pygame.init()
         self.screen = pygame.display.set_mode((1366, 768))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)
     def run_game(self):
 
         while True:
@@ -13,6 +15,7 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.bg_color)
 # Отображение последнего прорисованного экрана.
             pygame.display.flip()
 if __name__ == '__main__':
