dc9a60cef5647ad1b4ac19586a697a2ac3694fca
Sergey <myxu4@bk.ru>
2021-11-28 02:17:55 +0300

    Вывод коробля на экран

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,17 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 class AlienInvasion:
     def __init__(self):
-
-
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
         self.bg_color = (230, 230, 230)
+        
     def run_game(self):
 
         while True:
@@ -19,6 +20,8 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            
 # Отображение последнего прорисованного экрана.
             pygame.display.flip()
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ class Settings():
     def __init__(self):
 
 # Параметры экрана
-        self.screen_width = 1366
-        self.screen_height = 768
+        self.screen_width = 1200
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
