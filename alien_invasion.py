6d24260b08feaab9572218507b24edd52206a8d2
Sergey <myxu4@bk.ru>
2021-11-28 14:32:58 +0300

    Движение вверх\вниз

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,12 +30,16 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
-   
+
     def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_UP:#
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True#
         elif event.key == pygame.K_q:
             sys.exit()
     def _check_keyup_events(self, event):
@@ -43,6 +47,10 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+        elif event.key == pygame.K_DOWN:#
+            self.ship.moving_down = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False#
             
     def _update_screen(self):
         
