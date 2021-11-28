94c5e04a1dc3d397e840c0aa5f4333428f621e2a
Sergey <myxu4@bk.ru>
2021-11-28 11:55:57 +0300

    Регулировка скорости корабля

--- ship.py
+++ ship.py
@@ -4,18 +4,22 @@ class Ship():
 
         self.screen =ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 # Загружает изображение корабля и получает прямоугольник.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
         self.moving_right = False
         self.moving_left = False
+
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
     def blitme(self):
 
         self.screen.blit(self.image, self.rect)
