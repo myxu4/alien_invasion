b837a8151ec1f328e06a9a5273598db1e290ab7c
Sergey <myxu4@bk.ru>
2021-11-29 20:55:08 +0300

    Восстановление флотаВосстановление флота

--- alien_invasion.py
+++ alien_invasion.py
@@ -76,6 +76,9 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
         collisions = pygame.sprite.groupcollide(
                     self.bullets, self.aliens, True, True)
         if not self.aliens:
